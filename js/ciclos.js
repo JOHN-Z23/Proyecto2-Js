b38c103cc27116b2380bdb954f15587f62d7222a
JOHN-Z23 <john23zapata@gmail.com>
2022-02-20 12:44:18 -0300

    Creando ciclo

---
+++ js/ciclos.js
@@ -0,0 +1,31 @@
+let pais = prompt("¿Que pais desea visitar?");
+const ticket = "aqui está su ticket";
+
+while (pais != "") {
+  switch (pais.toUpperCase()) {
+    case "COLOMBIA":
+      const pasajes = prompt("Cuantos pasajes desea?");
+      if (pasajes < 3) {
+        alert(ticket)
+        pais = "";
+      } else {
+        const plata = prompt("No querés ir a Mar de plata");
+        if (plata.toUpperCase() != "SI") {
+          alert("Lo sentimos, no hay disponible la cantidad de pasajes requeridos")
+          pais = ""
+        } else {
+          alert(ticket)
+          pais = "";
+        }
+      }
+      break;
+    case "MAR DE PLATA":
+      alert(ticket)
+      pais = "";
+      break;
+    default:
+      alert("Esta pais no tiene pasajes disponibles");
+      pais = prompt("¿Que pais desea visitar?");
+      break;
+  }
+}
